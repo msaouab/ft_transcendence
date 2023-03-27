6284d258fe3a7f86eb0bd89fec5c8aab24503d74
ilyass qessam <khalid16oussama@gmail.com>
2023-03-27 14:06:12 +0000

    delete files

--- srcs/src/config/facebook.config.ts
+++
@@ -1,6 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export default registerAs('facebook', () => ({
-  appId: process.env.FACEBOOK_APP_ID,
-  appSecret: process.env.FACEBOOK_APP_SECRET,
-}));
--- srcs/src/config/google.config.ts
+++
@@ -1,6 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export default registerAs('google', () => ({
-  clientId: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-}));
