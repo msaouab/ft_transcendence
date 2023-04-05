0a58374b00578fb0341df10310e1eece7dfedf37
ilyass qessam <khalid16oussama@gmail.com>
2023-04-05 12:41:32 +0000

    clean env

--- srcs/requirements/Backend/src/dist/utils/cte.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.jwtSecret = void 0;
-exports.jwtSecret = process.env.JWT_SECRET;
-//# sourceMappingURL=cte.js.map
