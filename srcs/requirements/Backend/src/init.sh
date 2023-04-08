5065f26e690ada67aa6edd55b386349c40232ece
RidaEn-nasry <rida.ennasry@gmail.com>
2023-04-08 16:52:10 +0000

    remove healthcheck, adding a script to wait for postgres

--- srcs/requirements/Backend/src/init.sh
+++ srcs/requirements/Backend/src/init.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-npm install &&  npx prisma migrate dev --name dev --preview-feature
+until pg_isready -h $DATABASE_HOST -p 5432 -U $POSTGRES_USER
+do
+  echo "$(date) - waiting for database to start"
+  sleep 2
+done
+
+npm install
+npx prisma migrate dev --name dev --preview-feature
+npx prisma generate
+
 exec "$@"
