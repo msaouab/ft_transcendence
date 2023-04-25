c7dea2607257be92652e52d74ab4d3b526469293
Mohamed Behhar <mbehhar@e2r4p5.1337.ma>
2023-04-25 16:20:28 +0100

    setting up docker file

--- srcs/requirements/frontend/src/main.tsx
+++ srcs/requirements/frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
 import App from "./App.tsx";
 import "./index.scss";
+import { BrowserRouter } from "react-router-dom";
 
 axios.defaults.baseURL = import.meta.env.VITE_BACK_API;
 
