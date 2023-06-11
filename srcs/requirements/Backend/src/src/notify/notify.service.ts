9602fe4020420cb54f3379ef2ea5d3a9edfaf8f8
RidaEn-nasry <rida.ennasry@gmail.com>
2023-06-11 21:02:30 +0100

    removing id checking from handledisconnet

--- srcs/requirements/Backend/src/src/notify/notify.service.ts
+++ srcs/requirements/Backend/src/src/notify/notify.service.ts
@@ -1,4 +1,3 @@
-
 import { PrismaService } from "prisma/prisma.service";
 import { HttpException, Injectable } from "@nestjs/common";
 import { Prisma } from "@prisma/client";
@@ -6,38 +5,37 @@ import { User } from "@prisma/client";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 @Injectable()
-export class NotificationService  {
-        constructor(private prisma: PrismaService) { }
-        async updateUserStatus(userId: string, userStatus: boolean) {
-            if (!userId || userId === '') {
-                throw new HttpException('Invalid user id', 400);
-            }
-            const user = await this.prisma.user.findUnique({
-                where: {
-                    id: userId,
-                },
-            });
-            if (!user) {
-                console.log('user not found');
-                throw new HttpException('User not found', 404);
-            }
-            // beauty
-            try {
-               const user = await this.prisma.user.update({
-                    where: {
-                        id: userId,
-                    },
-                    data: {
-                        realStatus: userStatus,
-                    },
-                });
-                return user;
-            } catch (e) {
-                if (e instanceof PrismaClientKnownRequestError) {
-                    throw new HttpException(e.message, Number(e.code));
-                }
-                throw new HttpException(e.message, 500);
-            }
-        }
+export class NotificationService {
+	constructor(private prisma: PrismaService) {}
+	async updateUserStatus(userId: string, userStatus: boolean) {
+		// if (!userId || userId === '') {
+		//     throw new HttpException('Invalid user id', 400);
+		// }
+		const user = await this.prisma.user.findUnique({
+			where: {
+				id: userId,
+			},
+		});
+		if (!user) {
+			console.log("user not found");
+			throw new HttpException("User not found", 404);
+		}
+		// beauty
+		try {
+			const user = await this.prisma.user.update({
+				where: {
+					id: userId,
+				},
+				data: {
+					realStatus: userStatus,
+				},
+			});
+			return user;
+		} catch (e) {
+			if (e instanceof PrismaClientKnownRequestError) {
+				throw new HttpException(e.message, Number(e.code));
+			}
+			throw new HttpException(e.message, 500);
+		}
+	}
 }
-
